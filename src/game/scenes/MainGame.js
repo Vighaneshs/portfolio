f587c0cc228ce04e9db259648fdd1b77c1d27199
Vighaneshs <vighaneshsharma@gmail.com>
2024-05-23 01:26:31 +0530

    refining responsive movement

--- src/game/scenes/MainGame.js
+++ src/game/scenes/MainGame.js
@@ -10,7 +10,10 @@ export class MainGame extends Scene
     startMoving = false;
     isMoving = false;
     currPathIndex = 0;
+    isMovingWithPointer = false;
+    isSmallScreen = false;
 
+    playerSpeed = 125;
     constructor ()
     {
         super('MainGame');
@@ -23,32 +26,45 @@ export class MainGame extends Scene
         this.initMCAnim();
 
         this.initPlayer();
-        if(window.innerWidth < 800 || window.innerHeight < 550) {this.cameras.main.startFollow(this.player);}
+        
         
         this.showDebugWalls()
 
-        this.physics.add.collider(this.player, this.wallsLayer);
+        this.mainCollider = this.physics.add.collider(this.player, this.wallsLayer);
         
-        this.workexBox = this.physics.add.sprite(160, 244);  this.workexBox.setScale(1.25);
-        this.educationBox = this.physics.add.sprite(160, 410); this.educationBox.setScale(1.25);
-        this.projectBox = this.physics.add.sprite(640, 244); this.projectBox.setScale(1.25);
-        this.contactsBox = this.physics.add.sprite(350, 444); this.contactsBox.setScale(1.25);
+        this.workexBox = this.physics.add.sprite(160, 244);  this.workexBox.setScale(3,1.25)
+        this.educationBox = this.physics.add.sprite(160, 410); this.educationBox.setScale(3, 1.25);
+        this.projectBox = this.physics.add.sprite(640, 244); this.projectBox.setScale(3, 1.25);
+        this.contactsBox = this.physics.add.sprite(350, 444); this.contactsBox.setScale(3, 1.25);
         this.aboutBox = this.physics.add.sprite(690, 448); this.aboutBox.setScale(6, 5);
 
-        this.leftNullBox = this.physics.add.sprite(160, 327); this.leftNullBox.setScale(1.25, 2.65);
+        this.leftNullBox = this.physics.add.sprite(160, 327); this.leftNullBox.setScale(2, 2.65);
         this.rightNullbox = this.physics.add.sprite(690, 318); this.rightNullbox.setScale(6, 2);
-        this.middleNullBox = this.physics.add.sprite(350, 380); this.middleNullBox.setScale(1.25);
+        this.middleNullBox = this.physics.add.sprite(350, 380); this.middleNullBox.setScale(2);
 
-        this.target = new pMath.Vector2();
 
-        this.input.on('pointerdown', (pointer)=>{
-            this.currentPath = this.navMesh.findPath({x: this.player.body.x, y: this.player.body.y }, { x: pointer.x, y: pointer.y });
-            console.log(this.currentPath)
-            // this.target.x = 670
-            // this.target.y = 312
-            // this.physics.moveToObject(this.player, this.target, 1000);
+        
+
+        this.input.on('pointerdown', ()=>{
+            const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+            console.log(worldPoint.x, worldPoint.y)
+            this.currentPath = this.navMesh.findPath({x: this.player.body.x, y: this.player.body.y }, { x: worldPoint.x, y: worldPoint.y });
+            if(this.currentPath == null){
+                this.target = this.rectangleCoordFinder(worldPoint.x, worldPoint.y);
+                this.currentPath = this.navMesh.findPath({x: this.player.body.x, y: this.player.body.y }, { x: this.target.x, y: this.target.y });
+                if(this.currentPath == null){
+                    this.currentPath = this.navMesh.findPath({x: this.player.body.x-15, y: this.player.body.y }, { x: this.target.x, y: this.target.y });
+                    if(this.currentPath == null){
+                        this.currentPath = this.navMesh.findPath({x: this.player.body.x, y: this.player.body.y-15 }, { x: this.target.x, y: this.target.y });
+                        if(this.currentPath == null){
+                            this.currentPath = this.navMesh.findPath({x: this.player.body.x-15, y: this.player.body.y-15 }, { x: this.target.x, y: this.target.y });
+                        }
+                    }
+                }
+            }
             this.player.body.setVelocity(0);
             this.currPathIndex = 0;
+            if(this.mainCollider) this.mainCollider.active = false;
         }, this);
         this.cursors = this.input.keyboard.createCursorKeys()
         
@@ -57,32 +73,19 @@ export class MainGame extends Scene
 
     update (time, delta)
     {
-        //this.moveAndAnimate();
+        this.moveAndAnimate();
+        this.moveWithClick();
         this.checkOverlapsRooms();
-        //if(this.startMoving) this.movePlayerOnClick();
-
-        //console.log(this.pathPoint && this.currPathIndex < this.pathPoints.length)
-        if (this.currentPath && this.currPathIndex < this.currentPath.length){
-            if( Math.abs(this.player.body.x - this.currentPath[this.currPathIndex].x) < 1 && Math.abs(this.player.body.y - this.currentPath[this.currPathIndex].y) < 1){
-                this.currPathIndex += 1;
-                this.isMoving = false;
-                this.player.body.setVelocity(0);
-            } else if (!this.isMoving) {
-                // this.target.x =  this.currentPath[this.currPathIndex].x;
-                // this.target.y =  this.currentPath[this.currPathIndex].y;
-                // console.log(this.target.x, this.target.y)
-                // this.physics.moveToObject(this.player, this.target, 100);
-                let dirX =  (this.currentPath[this.currPathIndex].x - this.player.body.x)/Math.abs((this.currentPath[this.currPathIndex].x - this.player.body.x));
-                let dirY =  (this.currentPath[this.currPathIndex].y - this.player.body.y)/Math.abs((this.currentPath[this.currPathIndex].y - this.player.body.y));
-                let ratioX = ( Math.abs(this.currentPath[this.currPathIndex].x - this.player.body.x))/ (( Math.abs(this.currentPath[this.currPathIndex].x - this.player.body.x)) + ( Math.abs(this.currentPath[this.currPathIndex].y - this.player.body.y)));
-                let ratioY = ( Math.abs(this.currentPath[this.currPathIndex].y - this.player.body.y))/ (( Math.abs(this.currentPath[this.currPathIndex].x - this.player.body.x)) + ( Math.abs(this.currentPath[this.currPathIndex].y - this.player.body.y)));
-                this.player.body.setVelocityX(ratioX*dirX*100);
-                this.player.body.setVelocityY(ratioY*dirY*100);
-                this.isMoving = true;
-            }
+        if(window.innerWidth < 800 || window.innerHeight < 550) {
+            this.cameras.main.startFollow(this.player);
+            this.isSmallScreen = true;
         } else {
-            this.player.body.setVelocity(0);
+            this.cameras.main.stopFollow(this.player);
+            this.cameras.main.centerOn(416,288);
+            this.isSmallScreen = false;
         }
+        //console.log(this.pathPoint && this.currPathIndex < this.pathPoints.length)
+        
     
     }
 
@@ -111,7 +114,7 @@ export class MainGame extends Scene
         this.add.image(this.imageOfImageLayer.x, this.imageOfImageLayer.y, this.imageOfImageLayer.name).setOrigin(0,0);
         this.wallsLayer.setCollisionByProperty({ collided: true });
 
-        this.navMesh = this.navMeshPlugin.buildMeshFromTilemap("mesh", this.map, [this.wallsLayer]);
+        this.navMesh = this.navMeshPlugin.buildMeshFromTilemap("mesh", this.map, [this.wallsLayer], undefined, 4);
         // const path = navMesh.findPath({ x: 10, y: 10 }, { x: 392, y: 323 });
         // this.navMesh.enableDebug();
         // this.navMesh.debugDrawClear(); // Clears the overlay
@@ -129,7 +132,8 @@ export class MainGame extends Scene
         this.player.setScale(0.6);
         this.player.play('idle');
         this.player.body.setSize(38, 28);
-        this.player.body.setOffset(4, 70);
+        //this.player.body.setOffset(4, 70);
+        this.player.body.setOffset(23, 83);
     }
     initMCAnim()
     {   
@@ -165,53 +169,65 @@ export class MainGame extends Scene
         });
     }
 
-    
-    initMov(pointer){
-        this.pathPoints = this.navMesh.findPath({x:this.player.x, y:this.player.y}, {x:pointer.x, y:pointer.y});
-        if(this.pathPoints && this.pathPoints.length > 1) {
-            this.currPathIndex = 0;
-            this.startMoving = true
-        }
-    }
 
-    movePlayerOnClick(){
-        
-        if (this.currPathIndex < this.pathPoints.length-1) {
-            this.currPos = {x:this.player.body.x, y:this.player.body.y};
-            let dirX =  (this.pathPoints[this.currPathIndex].x - this.currPos.x)/Math.abs((this.pathPoints[this.currPathIndex].x - this.currPos.x));
-            let dirY =  (this.pathPoints[this.currPathIndex].y - this.currPos.y)/Math.abs((this.pathPoints[this.currPathIndex].y - this.currPos.y));
-            this.player.body.setVelocityX(100*dirX);
-            this.player.body.setVelocityY(100*dirY);
-            if (!dirX && !dirY) {
-                this.currPathIndex += 1;
-            }
-        } else {
-            this.player.body.setVelocity(0);
-            this.startMoving = false;
-        }
-    }
 
     moveAndAnimate(){
-        this.player.body.setVelocity(0);
-
+        if (this.cursors.left.isUp
+            && this.cursors.right.isUp
+            && this.cursors.down.isUp
+            && this.cursors.up.isUp
+            && !this.isMovingWithPointer)
+        {
+            this.player.body.setVelocity(0);
+            this.player.anims.play('idle', true);
+        }
+        if (this.isMovingWithKeyboard()){
+            this.mainCollider.active = true;
+            this.player.body.setOffset(4, 70);
+        } else{
+            this.player.body.setOffset(23, 83);
+        }
         // Horizontal movement
         if (this.cursors.left.isDown)
         {
-            this.player.body.setVelocityX(-100);
+            this.isMovingWithPointer = false;
+            if (this.cursors.up.isDown){
+                this.player.body.setVelocityY(-1*this.playerSpeed*Math.sin((45*Math.PI)/180));
+                this.player.body.setVelocityX(-1*this.playerSpeed*Math.sin((45*Math.PI)/180));
+            }
+            else if (this.cursors.down.isDown){
+                this.player.body.setVelocityY(this.playerSpeed*Math.sin((45*Math.PI)/180));
+                this.player.body.setVelocityX(-1*this.playerSpeed*Math.sin((45*Math.PI)/180));
+            }
+            else{
+                this.player.body.setVelocityY(0);
+                this.player.body.setVelocityX(-1*this.playerSpeed);
+            }
         }
         else if (this.cursors.right.isDown)
         {
-            this.player.body.setVelocityX(100);
-        }
-
-        // Vertical movement
-        if (this.cursors.up.isDown)
+            this.isMovingWithPointer = false;
+            if (this.cursors.up.isDown){
+                this.player.body.setVelocityY(-1*this.playerSpeed*Math.sin((45*Math.PI)/180));
+                this.player.body.setVelocityX(this.playerSpeed*Math.sin((45*Math.PI)/180));
+            }
+            else if (this.cursors.down.isDown){
+                this.player.body.setVelocityY(this.playerSpeed*Math.sin((45*Math.PI)/180));
+                this.player.body.setVelocityX(this.playerSpeed*Math.sin((45*Math.PI)/180));
+            }
+            else{
+                this.player.body.setVelocityY(0);
+                this.player.body.setVelocityX(this.playerSpeed);
+            }
+        } else  if (this.cursors.up.isDown)
         {
-            this.player.body.setVelocityY(-100);
+            this.isMovingWithPointer = false;
+            this.player.body.setVelocityY(-1*this.playerSpeed);
         }
         else if (this.cursors.down.isDown)
         {
-            this.player.body.setVelocityY(100);
+            this.isMovingWithPointer = false;
+            this.player.body.setVelocityY(this.playerSpeed);
         }
 
         // Update the animation last and give left/right animations precedence over up/down animations
@@ -231,9 +247,43 @@ export class MainGame extends Scene
         {
             this.player.anims.play('walkDown', true);
         }
-        else
-        {
-            this.player.anims.stop();
+    }
+
+    moveWithClick(){
+        if (this.currentPath && this.currPathIndex < this.currentPath.length){
+            if( Math.abs(this.player.body.x - this.currentPath[this.currPathIndex].x) < 5 && Math.abs(this.player.body.y - this.currentPath[this.currPathIndex].y) < 5){
+                this.currPathIndex += 1;
+                this.isMoving = false;
+                this.player.body.setVelocity(0);
+                this.isMovingWithPointer = false;
+            } else if (!this.isMoving) {
+                this.isMovingWithPointer = true;
+                let dirX =  (this.currentPath[this.currPathIndex].x - this.player.body.x)/Math.abs((this.currentPath[this.currPathIndex].x - this.player.body.x));
+                let dirY =  (this.currentPath[this.currPathIndex].y - this.player.body.y)/Math.abs((this.currentPath[this.currPathIndex].y - this.player.body.y));
+                let magnitude = Math.sqrt((Math.pow( Math.abs(this.currentPath[this.currPathIndex].x - this.player.body.x), 2)) + (Math.pow( Math.abs(this.currentPath[this.currPathIndex].y - this.player.body.y), 2)))
+                let ratioX = ( Math.abs(this.currentPath[this.currPathIndex].x - this.player.body.x))/ magnitude;
+                let ratioY = ( Math.abs(this.currentPath[this.currPathIndex].y - this.player.body.y))/ magnitude;
+                if(ratioX >= ratioY){
+                    if (dirX > 0){
+                        this.player.anims.play({key:'walkRight', repeat:-1});
+                    }else{
+                        this.player.anims.play({key:'walkLeft',repeat:-1});
+                    }
+                }else{
+                    if (dirY > 0){
+                        this.player.anims.play({key:'walkDown',repeat:-1});
+                    }else{
+                        this.player.anims.play({key:'walkUp',repeat:-1});
+                    }
+                }
+                this.player.body.setVelocityX(ratioX*dirX*this.playerSpeed);
+                this.player.body.setVelocityY(ratioY*dirY*this.playerSpeed);
+                this.isMoving = true;
+            }
+        } else if (!this.isMovingWithKeyboard()) {
+            this.isMovingWithPointer = false;
+            this.player.anims.play('idle', true);
+            this.player.body.setVelocity(0);
         }
     }
 
@@ -251,7 +301,53 @@ export class MainGame extends Scene
             this.inbox = false;
         }
     }
-    
+
+    isMovingWithKeyboard(){
+        if (this.cursors.left.isDown
+            ||this.cursors.right.isDown
+            ||this.cursors.up.isDown
+            ||this.cursors.down.isDown)
+            {
+                return true
+            }
+    }
+    //For path correction
+    rectangleCoordFinder(initPointerX, initPointerY ){
+        if(0 <= initPointerX && initPointerX <= 304 &&  0<= initPointerY && initPointerY <= 224){
+            //Work ex room
+            if (42 <= initPointerX <= 278) return new pMath.Vector2(initPointerX, 184);
+            if (initPointerX <= 42)  return new pMath.Vector2(42, 184);
+            return new pMath.Vector2(278, 184);
+        }else if(initPointerX >= 0 && initPointerY > 224 && initPointerX <= 304 && initPointerY <= 350){
+            //Path between workex and education
+            if (42 <= initPointerX <= 278) return new pMath.Vector2(initPointerX, 302);
+            return new pMath.Vector2(200, 302);
+        }else if(initPointerX >= 0 && initPointerY > 350 && initPointerX <= 304 && initPointerY <= 576){
+            //Education room
+            return new pMath.Vector2(158, 454);
+        }else if(initPointerX > 304 && initPointerY >= 0 && initPointerX <= 492 && initPointerY <= 92){
+            //Path between work and projects
+            return new pMath.Vector2(initPointerX, 104);
+        }else if(initPointerX > 304 && initPointerY >= 374 && initPointerX <= 558 && initPointerY <= 556){
+            //Contacts room
+            return new pMath.Vector2(451, 469);
+        } else if(initPointerX > 558 && initPointerY >= 374 && initPointerX <= 810 && initPointerY <= 556){
+            //About room
+            return new pMath.Vector2(684, 472);
+        }else if(initPointerX >= 492 && initPointerY > 224 && initPointerX <= 810 && initPointerY <= 374){
+            //Path between about and projects
+            return new pMath.Vector2(initPointerX, 302);
+        }else if(492 <= initPointerX && initPointerX <= 810 &&  0<= initPointerY && initPointerY <= 224){
+            //Work ex room
+            if (524 <= initPointerX <= 792) return new pMath.Vector2(initPointerX, 184);
+            if (initPointerX <= 524)  return new pMath.Vector2(524, 184);
+            return new pMath.Vector2(792, 184);
+        }
+        else {
+            return new pMath.Vector2(initPointerX, initPointerY);
+        }
+
+    }
 
     showDebugWalls() {
         if(this.showDebugW)
