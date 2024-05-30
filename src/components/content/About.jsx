d4f523294de9146cdcc71b83fa27f87e949c9c98
Vighaneshs <vighaneshsharma@gmail.com>
2024-05-30 21:57:25 +0530

    adding content on about page

--- src/components/content/About.jsx
+++ src/components/content/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <p className=' font-bold text-2xl border-2 border-orange-800 pl-4 align-middle'>ABOUT </p>
+        <div className='text-l text-left px-2 pt-4 space-y-2'>
+          <p  className='text-xl font-bold underline underline-offset-2'>Welcome to my portfolio!</p>
+          <li>Each room in this house reflects a part of my personal story.</li>
+          <li>Explore the rooms through the character to discover more about me.</li>
+          <li>Looking back on my programming journey from around ten years ago, I made the decision to craft my portfolio as a game, symbolizing my initial steps into the world of programming.
+          </li>
+            
+          <p className='text-lg font-bold underline underline-offset-2'>Controls</p>
+          <li>Touch/Click on the floor to move character.</li>
+          <li>You can also use Arrow Keys.</li>
+          <p className='text-xl font-bold pt-5'>Feedback is appreciated!</p>
+        </div>    
+    </div>
   )
 }
 
